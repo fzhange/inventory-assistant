fab71cb31a89449b55983eeedaf1569f87ee8aa4
fzhange <28616695353@qq.com>
2024-03-26 21:18:59 +0800

    feat: add service

--- inventory-assistant-service/dist/tools/index.helper.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function readExcelFile(filePath: any): unknown[];
-export declare function matchOrderAndInventory(cookedOrderData: any, cookedInventoryData: any): {};
-export * from './inventory.helper';
-export * from './order.helper';
--- inventory-assistant-service/dist/tools/order.helper.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function cookOrderData(rawOrderData: any): any[];
